8018e3627c360d31c7876391fd4cb0e24509b6c4
Hasibul Mahi <hasibulmahi@gmail.com>
2023-10-30 01:42:20 +0600

    Update index.js

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import Footer from '../footer/footer.component';
-import Navigation from '../header/navigation.component';
-import SectionTitle from '../section-title/section-title.component';
-import Services from '../services/services.component';
-import Hero from '../hero/hero.component';
+import React from "react";
+import Footer from "../footer/footer.component";
+import Navigation from "../header/navigation.component";
+import SectionTitle from "../section-title/section-title.component";
+import Services from "../services/services.component";
+import Hero from "../hero/hero.component";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
-function Layout({children}) {
-   
-    return (
-        <>
-        <Navigation/>
-        {children}
-        <Footer/>
-        </>
-    );
+function Layout({ children }) {
+  // const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  // const theme = React.useMemo(
+  //     () =>
+  //       createTheme({
+  //         palette: {
+  //           mode: prefersDarkMode ? 'dark' : 'light',
+  //         },
+  //       }),
+  //     [prefersDarkMode],
+  //   );
+
+  return (
+    <>
+      {/* <ThemeProvider theme={theme}> */}
+      <Navigation />
+      {children}
+      <Footer />
+      {/* </ThemeProvider> */}
+    </>
+  );
 }
 
 export default Layout;
